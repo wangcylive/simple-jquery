cd6603fe7af833d303ec2fe48fc3dcb73614372a
wangchunyang <chunyang_wang@istudy.com.cn>
2016-11-12 20:08:49 +0800

    commit

--- src/simple-jQuery.js
+++ src/simple-jQuery.js
@@ -170,8 +170,6 @@
     };
 
     function showHide(elements, show) {
-        var elem, style;
-
         for(var i = 0; i < elements.length; i++) {
             elements[i].style.display = show ? "" : "none";
         }
